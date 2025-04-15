d44dd1a8a94792dface82b92b7638750948d27ba
Ivo Curado <ivocurado@live.com.pt>
2025-04-15 19:04:48 +0100

    feat: Add UI to game

--- main.js
+++ main.js
@@ -12,56 +12,62 @@ function getComputerChoice() {
   return "Paper";
 }
 
-// console.log(getComputerChoice());
+let rockButton = document.createElement("button");
+rockButton.textContent = "Rock";
+let paperButton = document.createElement("button");
+paperButton.textContent = "Paper";
+let scissorsButton = document.createElement("button");
+scissorsButton.textContent = "Scissors";
 
-function getHumanChoice() {
-  let inputted = parseInt(
-    prompt("Choose: \n1 - Rock\n2 - Paper\n3 - Scissors")
-  );
-  switch (inputted) {
-    case 1:
-      return "Rock";
-    case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
-  }
+let body = document.querySelector("body");
+let placard = document.querySelector("#placard");
+placard.textContent = "Let's start a game";
+body.append(rockButton, paperButton, scissorsButton);
+body.addEventListener("click", (e) => {
+  playRound(getHumanChoice(e), getComputerChoice());
+});
+
+function getHumanChoice(e) {
+  return e.target.textContent;
 }
 
-// console.log(getHumanChoice());
-// console.log(getHumanChoice());
-
 function playRound(humanChoice, computerChoice) {
+  if (humanChoice >= 5 || computerChoice >= 5) return;
+  let topBanner = document.createElement("p");
   if (humanChoice == computerChoice) {
-    console.log("It's a tie for this round! Try again...");
+    topBanner.textContent = `It's a tie for this round! ${getScore()}`;
   } else if (
     (humanChoice == "Rock" && computerChoice == "Paper") ||
     (humanChoice == "Paper" && computerChoice == "Scissors") ||
     (humanChoice == "Scissors" && computerChoice == "Rock")
   ) {
     computerScore++;
-    console.error(`You lost this round!! :( ${getScore()}`);
-    return;
+    topBanner.textContent = `You lost this round!! :( ${getScore()}`;
   } else {
     humanScore++;
-    console.info(`You won this round!! :) ${getScore()}`);
+    topBanner.textContent = `You won this round!! :) ${getScore()}`;
   }
+  if (computerScore === 5 || humanScore === 5) {
+    topBanner.textContent = "";
+    showFinalResult();
+  }
+  placard.replaceChildren(topBanner);
 }
 
 function getScore() {
   return `(${humanScore} - ${computerScore})`;
 }
 
-function playGame() {
-  console.log("Game Started!");
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  console.log(
-    `Game Completed! You are the final ${
-      humanScore > computerScore ? "WINNER :)" : ( humanScore < computerScore ? "Loser :(" : 'Shared Winner')
-    } ${getScore()}`
-  );
+function showFinalResult() {
+  let resultBanner = document.createElement("p");
+  resultBanner.style.fontWeight = "bold";
+  resultBanner.style.fontSize = "32px";
+  resultBanner.textContent = `Game Completed! You are the final ${
+    humanScore > computerScore
+      ? "WINNER :)"
+      : humanScore < computerScore
+      ? "Loser :("
+      : "Shared Winner"
+  } ${getScore()}`;
+  body.appendChild(resultBanner);
 }
-
-playGame();
